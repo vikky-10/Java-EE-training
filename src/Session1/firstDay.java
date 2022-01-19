8f4d037dbbc3427c25451ebdd0be89c3d9912eb4
Vikky Singh <vikkyhp0007@gmail.com>
2022-01-19 23:23:54 +0530

    java sessions

---
+++ src/Session1/firstDay.java
@@ -0,0 +1,12 @@
+package Session1;
+
+public class firstDay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Thank You all for being a wonderful audience");
+		System.out.println("I will see ya tomorrow now");
+		System.out.println("Code the same what we did in the class");
+	}
+
+}
