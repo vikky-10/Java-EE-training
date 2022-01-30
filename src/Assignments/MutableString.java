1e9cf4794b9e8c24f9a6ea859cb2bbf7169de4ef
Vikky Singh <vikkyhp0007@gmail.com>
2022-01-30 23:09:04 +0530

    mutable string

---
+++ src/Assignments/MutableString.java
@@ -0,0 +1,51 @@
+package Assignments;
+
+public class MutableString {
+
+	public static void main(String[] args) {
+		 //StringBuffer
+//		StringBuffer str=new StringBuffer("vikky");
+//		System.out.println(str.length());
+//		str.append(" Singh");
+//		System.out.println(str);
+//		str.append(5);
+//		System.out.println(str);
+//		str.append(3.5);
+//		System.out.println(str);
+		
+		//insert(int offset, String s)  offset means string
+//		str.insert(2, "hii");
+//		System.out.println(str);
+//        str.insert(6, 2.58);
+//        System.out.println(str);
+		
+		//replace(int startindex, int endindex, String str)
+		
+		
+//		str.replace(0, str.length(), "Amit kumar");
+//		System.out.println(str);
+		
+		//delete(int startIndex, int endIndex)
+//		str.delete(5, str.length() );
+//		System.out.println(str);
+		
+		//reverse
+//		str.reverse();
+//		System.out.println(str);
+		
+		//capacity
+//		StringBuffer str1=new StringBuffer();
+//		StringBuilder str1=new StringBuilder();
+//		System.out.println(str1.capacity());//16
+//		str1.append("vikky");
+//		System.out.println(str1.capacity());//16
+//		str1.append(" singh");
+//		System.out.println(str1.capacity());//16
+//		str1.append(" increase");
+//		System.out.println(str1.capacity());//34
+		
+		
+		
+	}
+
+}
--- src/Assignments/StringMethods.java
+++ src/Assignments/StringMethods.java
@@ -22,22 +22,48 @@ public class StringMethods {
 //    System.out.println(s3.subSequence(1,6));
     
     
-    String s4="Ishant Sir";
-    String s5="ISHANT SIR";
-    String s6="Ishant Sir";
-    String s7="Vikky singh";
-    System.out.println(s4==s5);//false
-    
-    //case Sensitive
-    System.out.println(s4==s6);//true
-    System.out.println(s4==s7);//false
-    System.out.println(s4.equals(s5));//false
-    System.out.println(s4.equals(s6));//true
-    System.out.println(s4.equals(s7));//false
-    
-    
+//    String s4="Ishant Sir";
+//    String s5="ISHANT SIR";
+//    String s6="Ishant Sir";
+//    String s7="Vikky singh";
+//    System.out.println(s4==s5);//false
+//    
+//    //case Sensitive
+//    System.out.println(s4==s6);//true
+//    System.out.println(s4==s7);//false
+//    System.out.println(s4.equals(s5));//false
+//    System.out.println(s4.equals(s6));//true
+//    System.out.println(s4.equals(s7));//false
     
     
+//    concat
+//    String s8="amit";
+//    String s9=new String("jhon");
+//    s8.concat("singh");
+//    s9.concat("doe");
+//    System.out.println(s8);
+//    System.out.println(s9);
+//    System.out.println(s8+" " +s9);
+//    s8=s8+" "+ s9;// here what happend na new object is created inside heap
+//    System.out.println(s8);//amit jhon
+//    
+//    s8=s9;
+//    System.out.println(s8);//jhon
+//    
+//    s8="kumar";
+//    
+//    System.out.println(s8);//kumar
+//    
+//    s9="bhalu";
+//    System.out.println(s9);//bhalu
+//    
+//    
+//    System.out.println(s8);//kumar 
+//    System.out.println(s9);//bhalu
+//    
+  
+   
+     
     
     
 	}
